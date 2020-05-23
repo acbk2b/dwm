e78c544b6d8d27c293d3ea369e532f84c55c43c4
acbk2b <acbecker27@gmail.com>
2020-05-23 08:23:55 -0500

    Removed unused blocks

--- blocks.h
+++ blocks.h
@@ -2,13 +2,10 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
   {"🔋", "cat /sys/class/power_supply/BAT0/capacity", 30, 0},
-	//{"", "cat ~/.pacupdate | sed /📦0/d",					0,		9},
 	
 	{"", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
 
 	{"🔊", "amixer sget Master | awk '/Front Right:/{print $5}' | grep -o -E [[:digit:]]+\%",0,11},
-
-	//{"☀", "light",					0,		11},
 	
 	{"🌡", "sensors | awk '/^temp1:/{print $2}'",			10,		0},
 
