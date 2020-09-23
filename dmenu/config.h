082611b4e0387552df1b483e6f3f851b687e06e6
acbk2b <acbecker27@gmail.com>
2020-09-23 15:59:25 -0500

    Added dmenu files

---
+++ dmenu/config.h
@@ -0,0 +1,25 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+	"monospace:size=10"
+};
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char col_green[]       = "#00FF9C";
+static const char col_purple[]      = "#2E2837";
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+    [SchemeNorm] = { col_green, col_purple },
+	[SchemeSel]  = { col_purple, col_green },
+	[SchemeOut]  = { "#000000", "#00ffff" },
+};
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 0;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
