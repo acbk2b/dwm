05eb32be662bbc15f1b49fc5d2c7808a61976644
acbk2b <acbecker27@gmail.com>
2021-02-10 08:01:23 -0600

    Added movestack patch

--- dwm/config.h
+++ dwm/config.h
@@ -1,5 +1,7 @@
 /* See LICENSE file for copyright and license details. */
+
 #include <X11/XF86keysym.h>
+#include "extra-functions.c"
 
 /* Build changes from original 
  *
@@ -118,6 +120,8 @@ static Key keys[] = {
   { MODKEY,                       XK_b,                       togglebar,      {0} },
   { MODKEY,                       XK_j,                       focusstack,     {.i = +1 } },
   { MODKEY,                       XK_k,                       focusstack,     {.i = -1 } },
+  { MODKEY|ShiftMask,             XK_j,      movestack,      {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_k,      movestack,      {.i = -1 } },
 	{ MODKEY,                       XK_n,                       incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_v,                       incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_comma,                   setmfact,       {.f = -0.05} },
