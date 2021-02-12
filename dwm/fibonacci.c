deb5077919fb028647392651c2de95814058bdff
acbk2b <acbecker27@gmail.com>
2021-02-12 13:19:12 -0600

    Added link to original unmodified vanitygaps fibonacci patch patch

--- dwm/fibonacci.c
+++ dwm/fibonacci.c
@@ -1,3 +1,8 @@
+/*
+ * Taken and modified from the following patch to work with fullgaps instead of vanitygaps:
+ * https://dwm.suckless.org/patches/vanitygaps/dwm-vanitygaps-6.2.diff
+ */
+
 void
 getgaps(Monitor *m, int *oh, int *ov, int *ih, int *iv, unsigned int *nc)
 {
