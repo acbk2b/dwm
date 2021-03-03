7dfd833ea92da1f3a06367af816c464b98979f73
acbk2b <acbecker27@gmail.com>
2021-03-03 11:32:19 -0600

    Added pacman updates block to dwmblocks

--- dwmblocks/blocks.h
+++ dwmblocks/blocks.h
@@ -4,15 +4,25 @@
 
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+
+  // Packages available for update
+	{"", "echo \"Pkgs: $(yay -Qu | wc -l)\"",	7200,		0},
+
+  // Battery
 #if BATTERY
   {"", "if [[ $(cat /sys/class/power_supply/BAT0/status ) == 'Charging' ]]; then echo -n '^'; else echo -n 'V '; fi; cat /sys/class/power_supply/BAT0/capacity", 30, 0},
 #endif
+
+  // Ram usage
 	{"", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
 
-	{"", "amixer sget Master | awk '/Front Right:/{print $5}' | grep -o -E [[:digit:]]+\%",0,11},
-	
-	{"", "sensors | awk '/^temp1:/{print $2}'",			10,		0},
+  // Volume
+	{"", "amixer sget Master | awk '/Front Right:/{print $5}' | grep -o -E [[:digit:]]+\%",30,11},
 
+  // CPU Temp	
+	{"", "sensors | awk '/^temp1:/{print $2}'",			30,		0},
+
+  // Date and clock
 	{"", "date '+%a %e %b %k:%M:%S'",						1,		0},
 };
 
