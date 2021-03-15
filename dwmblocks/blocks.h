a1584b528760351ddcad452fedd5d94d79424153
acbk2b <acbecker27@gmail.com>
2021-03-15 11:15:58 -0500

    Moved dwmblocks blocks into separate scripts (copy to .local/bin)

--- dwmblocks/blocks.h
+++ dwmblocks/blocks.h
@@ -6,24 +6,24 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 
   // Packages available for update
-	{"", "echo \"Pkgs: $(yay -Qu | wc -l)\"",	3600,		0},
+	{"", "packages", 3600, 0},
 
   // Battery
 #if BATTERY
-  {"", "if [[ $(cat /sys/class/power_supply/BAT0/status ) == 'Charging' ]]; then echo -n '^'; else echo -n 'V '; fi; cat /sys/class/power_supply/BAT0/capacity", 30, 0},
+  {"", "battery",  30, 0},
 #endif
 
   // Ram usage
-	{"", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+	{"", "ram",	     30, 0},
 
   // Volume
-	{"", "amixer sget Master | awk '/Front Right:/{print $5}' | grep -o -E [[:digit:]]+\%",30,11},
+	{"", "volume",   30, 11},
 
   // CPU Temp	
-	{"", "sensors | awk '/^temp1:/{print $2}'",			30,		0},
+	{"", "cputemp",  30,	0},
 
   // Date and clock
-	{"", "date '+%a %e %b %k:%M:%S'",						1,		0},
+	{"", "datetime", 1,	0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
