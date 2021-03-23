2fe1fae6cb9259130253b5c7b5facd4321b4b96d
acbk2b <acbecker27@gmail.com>
2021-03-23 07:37:41 -0500

    Added dmenyu files

--- dmenu/config.def.h
+++ dmenu/config.def.h
@@ -5,16 +5,14 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"JetBrains Mono:size=9"
+	"monospace:size=10"
 };
-static const char *prompt      = "Run: ";      /* -p  option; prompt to the left of input field */
-static const char col_green[]       = "#00FF9C";
-static const char col_purple[]      = "#2E2837";
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-    [SchemeNorm] = { col_green, col_purple },
-	[SchemeSel]  = { col_purple, col_green },
-	[SchemeOut]  = { "#000000", "#00ffff" },
+	[SchemeNorm] = { "#bbbbbb", "#222222" },
+	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
