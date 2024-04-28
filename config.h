7324b5f661dad9ae0a612d4dad51136e0249cea5
Alex Becker <acbecker27@gmail.com>
2024-04-27 21:21:38 -0500

    Autostart st with tmux

--- config.h
+++ config.h
@@ -63,7 +63,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run" };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "st", "-e", "tmux", NULL };
 
 #include "movestack.c"
 static const Key keys[] = {
